b09aad93856fe11c6c7f57a2ee8e7dd5a19d67e1
Caryn McCarthy <cmccarthy15@gmail.com>
2020-10-05 16:27:03 -0400

    after intro enumerables lecture

--- 01-intro-enumerables/README.md
+++ 01-intro-enumerables/README.md
@@ -2,27 +2,47 @@
 
 ## Prerequisites
 
-- DataTypes 
+- DataTypes: strings, arrays, integer, booleans, hashes, floats, undefined, nil, char
+    - variables hold the info but are not inherently their own data type 
 - Conditionals 
+    - if/elsif/else/end
+    - if statements
+    - the condition innnn the while 
+    - case (switch)
+    - until's condition
+    - operators: and / or ==== && and ||, <, >, ==, !
+    - ternary (fiiiireeee) ==> next level 
 - Loops 
+    - while
+    - for loop
+    - do (code block)
+    - times
 - Arrays vs Hashes
 - Accessing Arrays & Accessing Hashes
 - Iterating through Arrays and Hashes using `.each`
 
 ## Agenda
 
-- Mindset
+- Mindset: don't just memorize (not the syntax, not nothing), understand the tool and when to use it
+    - focus on when to use what tool 
+    - get comfy reading the docs
 - Enumerables:
     - .each
-    - .map/.collect
-    - .filter/.select/.find_all
+    - .map && .collect  ==> a new array of the same length, with whatever the block returned for each element 
+        ==> all my spices in the bottles ===> a tsp of each spice
+    - .filter && .select  ===> a new array with the original elements for whom block returns true 
+        ===> all my spices ==> just the green ones 
     - .find
+        ===> all my spices ==> find cumin for me 
 - Pry
 - Show where this code lives on github and how to access it! 
     
 
 
 ## Icebox
+- Extra enumberables:
+    - .any, .all, .include?, .count/.length, .each_with_index, .sort, .reduce, .flatten, .detect
+- alias'es in your zshrc / bashprofile
 
 
 
--- 01-intro-enumerables/run.rb
+++ 01-intro-enumerables/run.rb
@@ -5,8 +5,59 @@ require 'pry'
 # 4. I want to be to find all pets that are dogs. What iterator should I use? What datatype should be returned?
 # 5. I want to be able to find a single dog with a specific name. How do I do that? What datatype should be returned?
 
+# 1. show a list of all the names of all my pets
+# 2. show me a list of just the pets that are... cats?
+# 3. let me search for pet by name
 
-def run 
-    puts "Hello World!"
+
+# tom - .each v .map
+
+all_animals = [
+    {name: "alpha", species:"lion", sound: "meow"},
+    {name: "Cloud", species: "cat", sound: "pteradactyl roar"}, # name, species, noise
+    {name: "Foxy Brown", species:"dog", sound: "ruff"},
+    {name: "Anise", species:"cat", sound: "mew"},
+    {name: "Nami", species:"dog", sound: "snore"},
+    {name: "alpha", species:"beta fish", sound: "bloop"}
+]
+
+# what if dealing with HoH? 
+
+# a collection of the same kind of info but no need to label each one ==> array
+# a collection of info where you need to label each piece ==> hash
+
+def run(animals)  # adding the run word to the dictionary metaphorically
+    puts "Welcome to the Pet Shop!!!"
+    puts "Please make a selection:"
+    puts "1. View a list of all animal names"
+    puts "2. View all cats" # view all cat names, not all cat instances
+    puts "3. Search for an animal by name"
+    choice = gets.chomp  # remove whitespace and newlines ie "1\n" vs "1"
+    
+    if choice == "1"
+        puts animals.map do |animal| #use do/end for multiline blocks, {} for single line
+            puts "saw #{animal[:name]}"
+            animal[:name]
+        end
+        # YOU CANNNN DO THIS WITH EACH BUT MAP/COLLECT IS BETTER
+        # just_names = []
+        # all_animals.each { |ele| just_names <<  ele[:name]}
+        # just_names
+    elsif choice == "2" 
+        puts animals.filter { |animal| animal[:species] == "cat" } # an array of cat hashes
+        # chaining enumberables! 
+        # puts animals.filter { |animal| animal[:species] == "cat" }.map { |cat| cat[:name]} # change this to print a list of cat names
+    elsif choice == "3"
+        puts "What is the animal's name?"
+        search = gets.chomp
+        puts animals.find { |ani| ani[:name] == search }
+    else
+        puts "Give me a valid choice please!"
+        run(animals)
+    end
+
+    # binding.pry # to stop me to inspect animals, choice etc
 end
 
+
+binding.pry # is to stop me so i can run run(all_animals)
