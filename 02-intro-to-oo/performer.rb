44cec8b8a1c03635645ac6a91bb66439d4b4b208
Michelle Rios <brendamichelle995@gmail.com>
2020-10-07 15:32:26 -0400

    after oops lecture pt 2

--- 02-intro-to-oo/performer.rb
+++ 02-intro-to-oo/performer.rb
@@ -1,15 +1,71 @@
+require 'pry'
+
 class Performer
 
     attr_reader :hit_songs
     attr_accessor :name, :talent, :genre
+    attr_accessor :networth
 
-    def initialize(name, talent, hit_songs,genre, height, networth)
+    # binding.pry
+    @@performer_count = 0
+    @@all = []
+
+    def initialize(name, talent, hit_songs, genre, height, networth)
         @name = name
         @talent = talent
         @hit_songs = hit_songs
         @genre = genre
         @height = height
         @networth = networth
+        @@performer_count += 1
+        # self.class.performer_count += 1
+        @@all << self
+        # self.class.all << self
+        # binding.pry
+    end
+
+    def self.all
+        @@all
+    end
+
+
+    # Return all the performers with a networth higher than 5000
+    # @@all
+    def self.high_networth_club
+        @@all.select do |performer|
+            performer.networth > 5000
+        end
+    end
+
+    def self.performer_count
+        @@performer_count
+    end
+
+
+    def collaborate(performer)
+        binding.pry
+        backstage
+        puts "#{self.name} is collaborating with #{performer.name}"
+    end
+
+    def praise
+        puts "#{self.name} is such an awesome performer!! <333"
+        # puts "#{@name} is such an awesome performer!! <333"
+    end
+
+    def perform
+        moonwalk_dance
+        increase_networth
+    end
+
+    private
+    def backstage
+        puts self
+        puts "this is a private method"
+    end
+
+    def increase_networth
+        self.networth += 600
     end
 
     def moonwalk_dance
--- 02-intro-to-oo/run.rb
+++ 02-intro-to-oo/run.rb
@@ -4,5 +4,10 @@ require 'pry'
 # name, talent, hit songs, age, genre, height, net worth
 p1 = Performer.new("Cher", "vocalist", "believe","pop", "tall", 10000)
 p2 = Performer.new("Britney", "vocalist", "OOPS", "pop", "average", 10000000000000)
+p3 = Performer.new("Bon Jovi", "vocalist", "OOPS", "pop", "average", 200)
+p4 = Performer.new("Selena", "vocalist", "OOPS", "pop", "average", 300)
+
+
+
 
 binding.pry
