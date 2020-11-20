c8c06c24ed65e22b84bfec3ecc5dcafaba6ff354
Michelle Rios <brendamichelle995@gmail.com>
2020-11-20 15:46:28 -0500

    after refactor, clean up a little

--- 26-toy-tale/src/index.js
+++ 26-toy-tale/src/index.js
@@ -14,39 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 
-  // const data = new FormData(form)
-  // const obj = {}
-  // data.forEach((value, key) => obj[key] = value)
-  // console.log(obj)
-
   /*** Variables ***/
   const addToyForm = document.querySelector(".add-toy-form")
   const toyCollection = document.querySelector("#toy-collection")
   const url = 'http://localhost:3000/toys'
 
 
-
-
   /*** Event Listeners ****/
-
   addToyForm.addEventListener('submit', (event) => {
     event.preventDefault()
-
     const data = new FormData(addToyForm)
     const obj = {}
     data.forEach((value, key) => obj[key] = value)
     obj.likes = 0
 
     makeFetchHappen('POST', url, obj)
-      .then((response) => response.json())
-      .then((toy) => {
-        renderOneToy(obj)
+      .then(response => response.json())
+      .then(toy => {
+        renderOneToy(toy)
         addToyForm.reset()
       })
   })
 
   toyCollection.addEventListener('click', (event) => {
-
     if (event.target.matches('button.like-btn')) {
       const div = event.target.closest('div')
       const id = div.dataset.id
@@ -61,12 +51,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   })
 
+
   /***** Helper Functions *****/
   function makeFetchHappen(method, url, jsObj) {
     return fetch(url, {
       method: method,
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Accept": "application/json"
       },
       body: JSON.stringify(jsObj)
     })
@@ -76,8 +68,8 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:3000/toys')
       .then(response => {
         return response.json()
-      }).then((toyArray) => {
-        toyArray.forEach((toy) => {
+      }).then(toyArray => {
+        toyArray.forEach(toy => {
           renderOneToy(toy)
         })
       })
