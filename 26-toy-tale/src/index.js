135a2bc630a25013da5752dd98a377f409d8379b
Michelle Rios <brendamichelle995@gmail.com>
2020-11-20 15:00:56 -0500

    complete, before refactor

--- 26-toy-tale/src/index.js
+++ 26-toy-tale/src/index.js
@@ -12,4 +12,89 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+  const addToyForm = document.querySelector(".add-toy-form")
+  const toyCollection = document.querySelector("#toy-collection")
+
+
+  fetch('http://localhost:3000/toys')
+    .then(response => {
+      return response.json()
+    }).then((toyArray) => {
+      toyArray.forEach((toy) => {
+        renderOneToy(toy)
+      })
+    })
+
+
+  addToyForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const toyName = event.target.name.value
+    // const toyName = addToyForm[0].value
+    const imgUrl = event.target.image.value
+    // const imgUrl = eaddToyForm[1].value
+    const toy = {
+      name: toyName,
+      image: imgUrl,
+      likes: 0
+    }
+
+    fetch('http://localhost:3000/toys', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(toy)
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((toy) => {
+        renderOneToy(toy)
+      })
+  })
+
+  toyCollection.addEventListener('click', (event) => {
+
+    if (event.target.matches('button.like-btn')) {
+      const div = event.target.closest('div')
+      const id = div.dataset.id
+      const likesDisplay = div.querySelector('p')
+      const newLikes = parseInt(likesDisplay.textContent) + 1
+
+      fetch(`http://localhost:3000/toys/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({ likes: newLikes })
+      })
+        .then(response => {
+          return response.json()
+        })
+        .then(toy => {
+          likesDisplay.textContent = `${toy.likes} likes`
+        })
+    }
+  })
+
+
+
+
+
+  function renderOneToy(toyObject) {
+    const div = document.createElement("div")
+    div.classList.add("card")
+    div.dataset.id = toyObject.id
+    div.innerHTML = `
+          <h2>${toyObject.name}</h2>
+          <img src=${toyObject.image} class="toy-avatar" />
+          <p>${toyObject.likes} Likes </p>
+          <button class="like-btn">Like <3</button>`
+    toyCollection.append(div)
+  }
+
 });
